c5fbfaa4576c55fd645297e47e8867ab0e6e085c
ShaneWhitmore <shane.s.whitmore@gmail.com>
2023-10-22 16:10:52 +0100

    Movie Details from API

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState, useEffect}  from "react";
+import { useParams } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -46,9 +48,11 @@ export default function MovieCard(props) {
         <IconButton aria-label="add to favorites" onClick={null}>
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
       </CardActions>
     </Card>
   );
